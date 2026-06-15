f03f6300a4a502c74176dd0511514fa2c72844ca
Cesar Mayora <cesarmay@protonmail.com>
2026-06-16 01:16:09 +0200

    renamed example

--- module/jni/Android.mk
+++ module/jni/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := example
-LOCAL_SRC_FILES := example.cpp
+LOCAL_MODULE := gotcha
+LOCAL_SRC_FILES := gotcha.cpp
 LOCAL_LDLIBS := -llog -lstdc++
 include $(BUILD_SHARED_LIBRARY)
