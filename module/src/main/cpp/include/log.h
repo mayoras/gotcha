042ff9f774468d0ca6f16d4e164bc3528d8b8ade
Cesar Mayora <cesarmay@protonmail.com>
2026-06-23 13:51:07 +0200

    fix: propagate debug definitions `GOTCHA_DEBUG` directly from CMAKE build type

--- module/src/main/cpp/include/log.h
+++ module/src/main/cpp/include/log.h
@@ -7,7 +7,11 @@
 
 #include <android/log.h>
 
+#if GOTCHA_DEBUG
+#define LOG_TAG "[GotchaDebug]"
+#else
 #define LOG_TAG "[Gotcha]"
+#endif
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
